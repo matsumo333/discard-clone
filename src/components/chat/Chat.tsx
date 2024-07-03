4fdf93a347a3d93c73e075e850e7ef05418681c7
matsmo333 <matsumo333@icloud.com>
2024-07-03 15:46:58 +0900

    フロントエンド完成

---
+++ src/components/chat/Chat.tsx
@@ -0,0 +1,39 @@
+import "./Chat.scss";
+import React from "react";
+import Chatheader from "./Chatheader";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
+import GifIcon from "@mui/icons-material/Gif";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
+import ChatMessage from "./ChatMessage";
+
+const Chat = () => {
+  return (
+    <div className="chat">
+      {/* chatheader */}
+      <Chatheader />
+      {/* chatmessage */}
+      <div className="chatMessage">
+        <ChatMessage />
+        <ChatMessage />
+        <ChatMessage />
+      </div>
+      <div className="chatInput">
+        <AddCircleOutlineIcon />
+        <form>
+          <input type="text" placeholder="#udemyへメッセージを送信" />
+          <button type="submit" className="chatInputButton">
+            送信
+          </button>
+        </form>
+        <div className="chatInputIcons">
+          <CardGiftcardIcon />
+          <GifIcon />
+          <EmojiEmotionsIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
