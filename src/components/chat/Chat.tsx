9077e28afff9a6653a880c9727e18cfb1dd4f5ab
matsmo333 <matsumo333@icloud.com>
2024-07-08 15:32:38 +0900

    an

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,5 +1,5 @@
 import "./Chat.scss";
-import React from "react";
+import React, { useState } from "react";
 import Chatheader from "./Chatheader";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
@@ -7,14 +7,46 @@ import GifIcon from "@mui/icons-material/Gif";
 import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
 import ChatMessage from "./ChatMessage";
 import { useAppSelector } from "../../app/hooks";
+import { inputAdornmentClasses } from "@mui/material";
+import {
+  addDoc,
+  collection,
+  CollectionReference,
+  DocumentData,
+  DocumentReference,
+  serverTimestamp,
+  Timestamp,
+} from "firebase/firestore";
+import { db } from "../../firebase";
 
 const Chat = () => {
+  const [inputText, setInputText] = useState<string>("");
   const channelName = useAppSelector((state) => state.channel.channelName);
-  console.log("ちゃと" + channelName);
+  const channelId = useAppSelector((state) => state.channel.channelId);
+  const user = useAppSelector((state) => state.user.user);
+  const sendMessage = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    //channelsコレクションの中にある
+    const collectionRef: CollectionReference<DocumentData, DocumentData> =
+      collection(db, "channels", String(channelId), "messages");
+    const docRef: DocumentReference<DocumentData> = await addDoc(
+      collectionRef,
+      {
+        message: inputText,
+        timeStamp: serverTimestamp(),
+        user: user,
+      }
+    );
+    console.log(docRef);
+  };
+
   return (
     <div className="chat">
       {/* chatheader */}
-      <Chatheader />
+      <Chatheader channelName={channelName} />
       {/* chatmessage */}
       <div className="chatMessage">
         <ChatMessage />
@@ -24,8 +56,20 @@ const Chat = () => {
       <div className="chatInput">
         <AddCircleOutlineIcon />
         <form>
-          <input type="text" placeholder="#udemyへメッセージを送信" />
-          <button type="submit" className="chatInputButton">
+          <input
+            type="text"
+            placeholder="#udemyへメッセージを送信"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setInputText(e.target.value)
+            }
+          />
+          <button
+            type="submit"
+            className="chatInputButton"
+            onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
+              sendMessage(e)
+            }
+          >
             送信
           </button>
         </form>
--- src/components/chat/Chatheader.tsx
+++ src/components/chat/Chatheader.tsx
@@ -6,13 +6,18 @@ import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 import SearchIcon from "@mui/icons-material/Search";
 import SendIcon from "@mui/icons-material/Send";
 import HelpIcon from "@mui/icons-material/Help";
-const Chatheader = () => {
+
+type Props = {
+  channelName: string | null;
+};
+const Chatheader = (props: Props) => {
+  const { channelName } = props;
   return (
     <div className="chatHeader">
       <div className="chatHeaderLeft">
         <h3>
           <span className="chatHeaderHash">#</span>
-          Udemy
+          {channelName}
         </h3>
       </div>
       <div className="chatHeaderRight">
